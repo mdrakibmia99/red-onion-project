c3965a30b2c961208d66447a7eaa32eb5192323f
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-07-18 23:12:59 +0600

    environment file problem solve

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,14 +5,22 @@ import { getAuth } from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_apiKey,
+//     authDomain: process.env.REACT_APP_authDomain,
+//     projectId: process.env.REACT_APP_projectId,
+//     storageBucket: process.env.REACT_APP_storageBucket,
+//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//     appId: process.env.REACT_APP_appId,
+// };
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
-};
+    apiKey: "AIzaSyAv-IyhccP7hiEjaQpmNBtpL7PT_ptkbpU",
+    authDomain: "red-onion-project-9c95a.firebaseapp.com",
+    projectId: "red-onion-project-9c95a",
+    storageBucket: "red-onion-project-9c95a.appspot.com",
+    messagingSenderId: "758525196773",
+    appId: "1:758525196773:web:791a08bcf850f9cb53170c"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
